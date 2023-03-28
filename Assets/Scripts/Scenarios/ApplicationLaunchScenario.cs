b03c0ddf71bd9c1f15b230447ab10835fe12f7f4
Egor_Shaldov <egor_shaldov@epam.com>
2023-03-28 16:24:27 +0300

    - Implemented 1st iteration of UIRoot - Implmented UIBuilder - Implmemented Main Menu Installer - Add stub for launch game scenario

---
+++ Assets/Scripts/Scenarios/ApplicationLaunchScenario.cs
@@ -0,0 +1,29 @@
+﻿using UI;
+using UnityEngine;
+using Zenject;
+
+namespace Scenarios
+{
+    public class ApplicationLaunchScenario : MonoBehaviour
+    {
+        private UIRoot _uiRoot;
+        
+        [Inject]
+        public void Construct(UIRoot uiRoot)
+        {
+            _uiRoot = uiRoot;
+        }
+        
+        protected void Start()
+        {
+            //var view = _uiRoot.Open<StartSplashScreen>();
+            // view.Closed += OnViewClosed;
+        }
+        
+        private void OnSplashScreenClosed(IView view)
+        {
+            view.Closed -= OnSplashScreenClosed;
+            // _uiRoot.Open<MainMenu>();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Common/UIBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace UI
+{
+    public class UIBuilder : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _viewsPrefab;
+        
+        private DiContainer _container;
+        private readonly Dictionary<Type, List<IView>> _pool = new();
+        
+        
+        [Inject]
+        public void Construct(DiContainer container)
+        {
+            _container = container;
+        }
+        
+        public TView Build<TView>(Transform parent) where TView : IView
+        {
+            var viewPrefab = _viewsPrefab.Find(x => x.GetComponent<TView>() != null);
+
+            if (viewPrefab == null)
+            {
+                Debug.LogError($"View{typeof(TView)} not found. Builder: {name}");
+                return default;
+            }
+
+            return _container.InstantiatePrefab(viewPrefab, parent).GetComponent<TView>();   
+        }
+
+        public void ToPool<TView>(TView view) where TView : IView
+        {
+            if(_pool.TryGetValue(typeof(TView), out var list))
+            {
+                list.Add(view);
+                return; 
+            }
+
+            _pool.Add(typeof(TView), new List<IView> {view});
+        }
+    }
+}
