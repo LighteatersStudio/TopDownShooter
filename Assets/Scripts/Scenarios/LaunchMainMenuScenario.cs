26c8caac11b945268b8f35f77bcb253a34e16abf
frnzee <102321734+frnzee@users.noreply.github.com>
2023-03-31 13:29:00 +0200

    Refactoring

--- Assets/Scripts/Scenarios/LaunchMainMenuScenario.cs
+++ Assets/Scripts/Scenarios/LaunchMainMenuScenario.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using Cysharp.Threading.Tasks;
-using Loading;
-using UI;
+﻿using UI;
 using UnityEngine;
 using Zenject;
 
@@ -11,47 +8,23 @@ namespace Scenarios
     {
         private IUIRoot _uiRoot;
 
-        private LoadingService _loadingService;
-        private LevelLoadingOperation _levelLoadingOperation;
-        
         [Inject]
-        public void Construct(IUIRoot uiRoot, LoadingService loadingService, LevelLoadingOperation levelLoadingOperation)
+        public void Construct(IUIRoot uiRoot)
         {
             _uiRoot = uiRoot;
-            _loadingService = loadingService;
-            _levelLoadingOperation = levelLoadingOperation;
         }
-
 
         protected void Start()
         {
-            var mainMenuView = _uiRoot.Open<MainMenu>();
-            mainMenuView.Closed += OnMainMenuClosed;
-            
             var startSplashScreenView = _uiRoot.Open<StartSplashScreen>();
             startSplashScreenView.Closed += OnSplashScreenClosed;
         }
-
-        // Debug code
-        private async void  WaitAndLoadLevel()
-        {
-            await UniTask.Delay(5000);
-
-            await _loadingService.Load(_levelLoadingOperation);
-        }
-
+        
         private void OnSplashScreenClosed(IView view)
         {
             view.Closed -= OnSplashScreenClosed;
             _uiRoot.Open<MainMenu>();
-        }
-        
-        private void OnMainMenuClosed(IView view)
-        {
-            view.Closed -= OnMainMenuClosed;
-            WaitAndLoadLevel();
             Destroy(gameObject);
         }
-
     }
 }
--- Assets/Scripts/UI/Popups/MainMenu.cs
+++ Assets/Scripts/UI/Popups/MainMenu.cs
@@ -1,6 +1,44 @@
-﻿namespace UI
+﻿using Loading;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI
 {
     public class MainMenu : View
     {
+        [SerializeField] private Button _playButton;
+        
+        private IUIRoot _uiRoot;
+        private LoadingService _loadingService;
+        private LevelLoadingOperation _levelLoadingOperation;
+
+        [Inject]
+        public void Construct(LoadingService loadingService, LevelLoadingOperation levelLoadingOperation)
+        {
+            _loadingService = loadingService;
+            _levelLoadingOperation = levelLoadingOperation;
+        }
+
+        private void OnEnable()
+        {
+            _playButton.onClick.AddListener(LoadLevel);
+        }
+
+        private void OnDisable()
+        {
+            _playButton.onClick.RemoveListener(LoadLevel);
+        }
+
+        private void LoadLevel()
+        {
+            _loadingService.Load(_levelLoadingOperation);
+        }
+
+        private void OnMainMenuClosed(IView view)
+        {
+            view.Closed -= OnMainMenuClosed;
+            Destroy(gameObject);
+        }
     }
 }
