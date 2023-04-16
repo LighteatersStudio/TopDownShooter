20d910d251b328aa4a1817972ae9f12f19e64599
Egor_Shaldov <egor_shaldov@epam.com>
2023-04-16 13:43:40 +0300

    - drop code

---
+++ Assets/Scripts/Gameplay/Character/Character.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class Character : MonoBehaviour
+    {
+        private static int HashSpeed = Animator.StringToHash("Speed");
+        
+        private CharacterStats _stats;
+        public ICharacterStats Stats => _stats;
+
+
+        [SerializeField] private Animator _animator; 
+        
+        private void Init()
+        {
+            _stats = new CharacterStats();       
+        }
+        
+        public void TakeDamage(float damage)
+        {
+            _stats.ApplyDamage(damage);
+        }
+            
+        protected void Update()
+        {
+            
+            _animator.SetFloat("Health", _stats.Health);
+        }
+    }
+    
+    public interface ICharacterMovement
+    {
+        public float Speed { get; }
+    }
+
+
+    public interface ICharacterStats
+    {
+        public float MaxHealth { get; }
+        public float Health { get; }
+    }
+    
+    public class CharacterStats : ICharacterStats
+    {
+        public float MaxHealth { get; private set; }
+        public float Health { get; private set; }
+        
+        public CharacterStats()
+        {
+            MaxHealth = 100f;
+            Health = MaxHealth;
+        }
+        
+        public void ApplyDamage(float damage)
+        {
+            Health -= damage;
+        }
+    }
+}
