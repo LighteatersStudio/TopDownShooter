c4c6b55ed6b81a9cc2038e60a3b7098abc05b9c3
Kjusok <103447181+Kjusok@users.noreply.github.com>
2023-04-17 21:46:39 +0300

    Added Prefab EndPoint
    Added Prefab EndPoint.
    When crossing with "Player", an event occurs - LevelPassed.

---
+++ Assets/Scripts/Gameplay/EndPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class EndPoint : MonoBehaviour
+    {
+        public event Action LevelPassed;
+        
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Player>())
+            {
+                LevelPassed?.Invoke();
+            }
+        }
+    }
+}
