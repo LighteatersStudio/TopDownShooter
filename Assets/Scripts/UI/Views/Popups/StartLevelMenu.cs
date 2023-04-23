49957a49c177f54c4cbfdc14132a7d061644b560
Kjusok <103447181+Kjusok@users.noreply.github.com>
2023-04-23 21:13:49 +0300

    Added StartLevelMenu
    Added StartLevelMenu

--- Assets/Scripts/UI/Views/Popups/StartLevelMenu.cs
+++ Assets/Scripts/UI/Views/Popups/StartLevelMenu.cs
@@ -1,9 +1,28 @@
-﻿using System;
+﻿using Zenject;
+using Services.Pause;
 
 namespace UI
 {
     public class StartLevelMenu : PopupBase
     {
+        private IPause _pauseManager;
+
+        [Inject]
+        public void Construct(IPause pause)
+        {
+            _pauseManager = pause;
+        }
         
+        
+        private void Start()
+        {
+            _pauseManager.Paused = true;
+        }
+
+        public void ClickGoButton()
+        {
+            _pauseManager.Paused = false;
+            Close();
+        }
     }
 }
