39d46c716ed7cf75869f918ab661d26b2737ea09
Egor_Shaldov <egor_shaldov@epam.com>
2023-05-21 10:41:08 +0300

    - Implemented IColorScheme - Implmented choice color scheme on high\stone selector - Update 1stLevel: apply color scheme for main source of light and wall - Added Finish Run for Pause Menu + update prefab - Refactored GameRun

---
+++ Assets/Scripts/Coloring/ColorSchemeSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Coloring
+{
+    [CreateAssetMenu(menuName = "LightEaters/Coloring/Create ColorSchemeSettings", fileName = "ColorSchemeSettings", order = 0)]
+    public class ColorSchemeSettings : ScriptableObject
+    {
+        [SerializeField] private ColorSchema _default;
+        [SerializeField] private ColorSchema _high;
+        [SerializeField] private ColorSchema _stone;
+        
+        public IColorSchema Default => _default;
+        
+        public IColorSchema High => _high;
+        public IColorSchema Stone => _stone;
+    }
+}
---
+++ Assets/Scripts/Coloring/Schema/IColorSchema.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Coloring
+{
+    public interface IColorSchema
+    {
+        Color Main { get; } 
+        Color Second { get; } 
+        Color Third { get; }
+    }
+}
