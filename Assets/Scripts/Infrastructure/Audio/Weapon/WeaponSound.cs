d21a098b5ed48078b8868ce6f338f0a177e3547d
egor.shaldov <egor.shaldov@gmail.com>
2023-06-29 14:04:59 +0300

    Task completed: https://lighteatersstudio.atlassian.net/browse/GP-246 https://lighteatersstudio.atlassian.net/browse/GP-236
    Implemented Shot sound event
    Implemented Reload sound event
    Added new weapon sound

---
+++ Assets/Scripts/Infrastructure/Audio/Weapon/WeaponSound.cs
@@ -0,0 +1,58 @@
+using Gameplay.Weapons;
+using UnityEngine;
+using Zenject;
+
+namespace Services.Audio.Weapon
+{
+    public class WeaponSound : MonoBehaviour
+    {
+        [SerializeField] private ExtendedAudioClip _pickingUp;
+        [SerializeField] private ExtendedAudioClip _shot;
+        [SerializeField] private ExtendedAudioClip _reload;
+        
+        private IWeapon _owner;
+        private IAudioPlayer _audioPlayer;
+
+        private void Awake()
+        {
+            _owner = GetComponent<IWeapon>();
+            if (_owner == null)
+            {
+                Debug.LogError("Weapon not founded");
+            }
+        }
+        
+        [Inject]
+        public virtual void Construct(IAudioPlayer audioPlayer)
+        {
+            _audioPlayer = audioPlayer;
+        }
+
+        private void Start()
+        {
+            _owner.ShotDone += OnShotDone;
+            _owner.ReloadStarted += OnReloadStarted;
+                
+            OnPickup();
+        }
+
+        private void OnDestroy()
+        {
+            _owner.ShotDone -= OnShotDone;
+            _owner.ReloadStarted -= OnReloadStarted;
+        }
+
+        private void OnShotDone()
+        {
+            _audioPlayer.PlayOneShoot(_shot);
+        }
+        private void OnReloadStarted()
+        {
+            _audioPlayer.PlayOneShoot(_reload);
+        }
+        private void OnPickup()
+        {
+            _audioPlayer.PlayOneShoot(_pickingUp);
+        }
+    }
+}
