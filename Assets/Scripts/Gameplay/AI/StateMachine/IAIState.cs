0240f8bb47cd7670372d6bfe958e60252c67b909
egor.shaldov <egor.shaldov@gmail.com>
2023-08-10 08:15:34 +0300

    - separate class to self file

--- Assets/Scripts/Gameplay/AI/StateMachine/IAIState.cs
+++ Assets/Scripts/Gameplay/AI/StateMachine/IAIState.cs
@@ -6,16 +6,4 @@ namespace Gameplay.AI
     {
         Task<StateResult> Launch();
     }
-    
-    public class StateResult
-    {
-        public IAIState NextState { get; }
-        public bool IsFinished { get; }
-
-        public StateResult(IAIState nextState, bool isFinished)
-        {
-            NextState = nextState;
-            IsFinished = isFinished;
-        }
-    }
 }
