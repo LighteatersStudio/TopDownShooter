5dbc54c349787b604a960c296bedd1dc16531554
Kjusok <103447181+Kjusok@users.noreply.github.com>
2023-08-20 21:56:33 +0300

    Task completed: GP-291. ObserverArea and Behavior
    Implemented behavior: when there is no movement, the Observer Area moves from right to left

--- Assets/Scripts/Gameplay/AI/Systems/Attacking/ObserveArea.cs
+++ Assets/Scripts/Gameplay/AI/Systems/Attacking/ObserveArea.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Gameplay.AI
 {
     public class ObserveArea : MonoBehaviour
     {
-        public event Action TargetsChanged;
+        private const float MovementThreshold = 0.01f;
+        
+        [SerializeField][Range(0,360)] private int _angel = 30;
+        [SerializeField] private int _rotationSpeed = 2;
 
         private readonly List<Transform> _targetsTransforms = new();
-
+        
+        private Tween _rotationTween;
+        private Vector3 _lastPosition;
+        private bool _isMoving;
+        
+        public event Action TargetsChanged;
         public bool HasTarget => _targetsTransforms.Count > 0;
-
         public IEnumerable<Transform> TargetsTransforms => _targetsTransforms;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.GetComponent<Player>())
@@ -26,8 +35,52 @@ namespace Gameplay.AI
         {
             if (other.gameObject.GetComponent<Player>() && _targetsTransforms.Count != 0)
             {
-                _targetsTransforms.Clear();
+                _targetsTransforms.Remove(other.gameObject.transform);
                 TargetsChanged?.Invoke();
+            }
+        }
+
+        private void Start()
+        {
+            _lastPosition = transform.position;
+        }
+        
+        private void RotationRight()
+        {
+            _rotationTween = transform.DOLocalRotate(new Vector3(0f, _angel, 0f), _rotationSpeed, RotateMode.Fast)
+                .OnComplete(RotationLeft);
+        }
+        
+        private void RotationLeft()
+        {
+            _rotationTween = transform.DOLocalRotate(new Vector3(0f, -_angel, 0f), _rotationSpeed, RotateMode.Fast)
+                .OnComplete(RotationRight);
+        }
+        
+        private void KillRotationTween()
+        {
+            _rotationTween.Kill();
+            transform.rotation = new Quaternion();
+        }
+
+        private void Update()
+        {
+            _isMoving = Mathf.Abs(transform.position.magnitude - _lastPosition.magnitude) > MovementThreshold ;
+            _lastPosition = transform.position;
+
+            ProcessRotation();
+        }
+
+        private void ProcessRotation()
+        {
+            if (_isMoving && _rotationTween.IsActive())
+            {
+                KillRotationTween();
+                _isMoving = false;
+            }
+            else if (!_isMoving && !_rotationTween.IsActive())
+            {
+                RotationRight();
             }
         }
     }
