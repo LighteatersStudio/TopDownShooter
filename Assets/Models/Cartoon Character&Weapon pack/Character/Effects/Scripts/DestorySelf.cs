762545f975c69e3579a9ead978432dae12c1e3da
egor.shaldov <egor.shaldov@gmail.com>
2023-10-30 21:08:08 +0200

    - added 2nd character pack asset

---
+++ Assets/Models/Cartoon Character&Weapon pack/Character/Effects/Scripts/DestorySelf.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestorySelf : MonoBehaviour {
+
+	public float dtime = 2.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dtime -= Time.deltaTime;
+		if(Time.frameCount % 10 == 0)
+			if(dtime < 0.0f)
+				Destroy(this.gameObject);
+	}
+}
